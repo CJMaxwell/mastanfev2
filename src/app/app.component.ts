28d6353b33a465f4c22ed88538efe99e8d3658a1
Maxwell Chukwunonyerem <maxwellchukwunonyerem@Maxwells-MacBook-Pro.local>
2023-08-03 19:31:39 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'stan-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'mastanfe';
+}
